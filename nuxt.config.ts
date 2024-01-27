8d8936665117c9ef26aa4e02561dfac430c416f6
pdTrade <pd.pub.dev@gmail.com>
2024-01-27 20:00:04 +0900

    typo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      WEATHER_APP_SECRET: process.env.WEATHER_API_Key,
+      WEATHER_APP_SECRET: process.env.WEATHER_API_KEY,
     }
   },
   css: ['~/assets/css/tailwind.css'],
